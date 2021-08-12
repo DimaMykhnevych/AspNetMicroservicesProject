6d4a76116a2fffc41ce99e7964d666c83a09bc0d
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-08-12 22:44:44 +0300

    Implement Application Contracts

---
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+
+namespace Ordering.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+        }
+    }
+}
