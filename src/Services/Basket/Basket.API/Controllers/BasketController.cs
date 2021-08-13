a6a3e7c7ce227f5022a0602af164d8fa931f85f7
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-08-13 07:49:24 +0300

    Remove unnecessary usings

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -33,10 +33,6 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
-            // TODO : Communicate with Discount.Grpc
-            // Calculate latest prices of product in shopping cart
-            // consume Discount Grpc
-
             foreach(var item in basket.Items)
             {
                 var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
